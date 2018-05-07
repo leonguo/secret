12c23deb152386d4a1f8cf1b37f2bca081d92c0d
guoxingjun <hotman8168@gmail.com>
2018-05-07 16:48:58 +0800

    db gorm

--- app/controllers/userController.go
+++ app/controllers/userController.go
@@ -11,5 +11,9 @@ func GetUser(c echo.Context) error {
 	users := new(models.User)
 	users.GetUserById(1)
 	spew.Dump(users)
+	test := make([]map[string]interface{},0)
+	m := map[string]interface{}{"dd": "dd"}
+	test = append(test, m)
+	spew.Dump(test)
 	return c.JSON(http.StatusOK, users)
 }
